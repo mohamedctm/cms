568b5bb43733bd2e0f241c9fa41b926d9bcd7c48
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2020-06-18 04:57:51 -0400

    Add files via upload
    e

--- src/main/java/com/project2/cms/Shadow1Application.java
+++ src/main/java/com/project2/cms/Shadow1Application.java
@@ -1,15 +1,9 @@
 package com.project2.cms;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,22 +19,19 @@ public class Shadow1Application {
 
 
 
-
-	@Bean
-	public FilterRegistrationBean processCorsFilter() {
-	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	final CorsConfiguration config = new CorsConfiguration();
-	config.setAllowCredentials(true);
-	config.addAllowedOrigin("'");
-	config.addAllowedHeader("*");
-	config.addAllowedMethod("*");
-	source.registerCorsConfiguration("/**", config);
-
-
-	final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter());
-	bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	return bean;
-	}
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+  // We're defining the class we're using inline here as a shortcut.
+  // You could create a separate file
+  return new WebMvcConfigurer() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE",
+          "OPTIONS").allowedOrigins("https://localhost:3000");
+    }
+    
+  };
+}
 
 
 
