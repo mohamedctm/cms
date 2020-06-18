18b6a155b5efdd1c7185a97140828e600e8d8163
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2020-06-18 03:58:28 -0400

    Update Shadow1Application.java
    cors again

--- src/main/java/com/project2/cms/Shadow1Application.java
+++ src/main/java/com/project2/cms/Shadow1Application.java
@@ -3,10 +3,14 @@ package com.project2.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
+@EnableWebMvc
 public class Shadow1Application {
 
 	public static void main(String[] args) {
