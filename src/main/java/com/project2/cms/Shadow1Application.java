37b7aea47e3b5c9710e0dc7f4eeb65a37a41e6ea
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2020-06-18 03:52:20 -0400

    Update Shadow1Application.java
    cors -- added

--- src/main/java/com/project2/cms/Shadow1Application.java
+++ src/main/java/com/project2/cms/Shadow1Application.java
@@ -2,6 +2,9 @@ package com.project2.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Shadow1Application {
@@ -9,5 +12,20 @@ public class Shadow1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Shadow1Application.class, args);
 	}
+
+
+
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+  // We're defining the class we're using inline here as a shortcut.
+  // You could create a separate file
+  return new WebMvcConfigurer() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE",
+          "OPTIONS");
+    }
+  };
+}
 
 }
